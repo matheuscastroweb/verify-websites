b1f80d6fd1152cfbc319bfab95febac8f18faead
Matheus Castro <matheuscastroweb@gmail.com>
2019-12-12 09:22:13 -0300

    Alterações no código

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,10 @@
+## [v0.0.1]
+> Dez 12, 2019
+
+- Alterado pasta do log para raiz
+- Adicionado a pasta `pages` que receberá o HTML da página com erro
+- Alterado nome do arquivo de erro para `logs.txt`
+
 ## [v0.0.0]
 > Dez 11, 2019
 
--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-const http = require('http');
-const https = require('https');
-const colors = require('colors/safe');
+const http = require("http");
+const https = require("https");
+const colors = require("colors/safe");
 const fs = require("fs");
 
 const status = async (arraySites) => {
-    //console.time('#forEach');
+    //console.time("#forEach");
 
     var arrayJson = JSON.parse(arraySites);
 
     arrayJson.forEach(function (website) {
 
-        var protocol = website.url.split(':');
+        var protocol = website.url.split(":");
 
-        var method = protocol[0] === 'http' ? http : https;
+        var method = protocol[0] === "http" ? http : https;
 
         //Método asyncrono
         method.get(website.url, (resp) => {
@@ -22,38 +22,32 @@ const status = async (arraySites) => {
             //if (statusCode == 200) {
             //   successcode = successcode + 1
             // }
-            // const contentType = resp.headers['content-type'];
+            //const contentType = resp.headers["title"];
 
-            let data = '';
+            let data = "";
 
-            resp.on('data', (chunk) => {
+            resp.on("data", (chunk) => {
                 data += chunk;
             });
 
-            resp.on('end', () => {
+            resp.on("end", () => {
                 if (statusCode !== 200 && status !== 301) {
-                    var data = new Date();
+                    var date = new Date();
 
-                    var statusText = "\n" + data
+                    var statusText = "\n" + date
                     statusText += "\n" + "Nome: " + website.nome + "\n" + "Url: " + website.url + "\n" + "Responsável: " + website.responsavel + "\n" + "Status: " + statusCode + "\n"
-                    fs.appendFileSync('errors.txt', statusText);
+                    fs.appendFileSync("logs.txt", statusText);
 
+                    fs.appendFileSync("pages/" + website.nome + ".html", data);
+
+                    console.log("");
+                    console.log("Nome: " + website.nome);
+                    console.log("Url: " + website.url);
+                    console.log("Responsável: " + website.responsavel);
+                    console.log("STATUS: " + colors.red(statusCode));
+                    console.log("");
                 }
 
-                //console.log(resp.statusCode);
-
-                //Retorna o código do site completo
-                // console.log(data);
-
-                //Retorna o status HTTP
-
-                console.log('');
-                console.log('Nome: ' + website.nome);
-                console.log('Url: ' + website.url);
-                console.log('Responsável: ' + website.responsavel);
-                console.log('STATUS: ' + colors.green(statusCode));
-                console.log('');
-                //console.log(contentType);
             });
 
         }).on("error", (err) => {
@@ -63,23 +57,17 @@ const status = async (arraySites) => {
             var statusText = "\n" + data
             statusText += "\n" + "Nome: " + website.nome + "\n" + "Url: " + website.url + "\n" + "Responsável: " + website.responsavel + "\n" + "Status: " + err.message + "\n"
 
-            fs.appendFileSync('errors.txt', statusText);
-
+            fs.appendFileSync("logs.txt", statusText);
 
             //Retorna o status HTTP
-            console.log('');
-            console.log('Nome: ' + website.nome);
-            console.log('Url: ' + website.url);
-            console.log('Responsável: ' + website.responsavel);
-            console.log('STATUS: ' + colors.red(err.message));
-            console.log('');
-            //console.log(contentType);
+            console.log("");
+            console.log("Nome: " + website.nome);
+            console.log("Url: " + website.url);
+            console.log("Responsável: " + website.responsavel);
+            console.log("STATUS: " + colors.red(err.message));
         });
-
-
     })
-    //console.timeEnd('#forEach');
+    //console.timeEnd("#forEach");
 }
-
 
 module.exports = { status }
